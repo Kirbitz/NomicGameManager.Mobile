8b07ab5fe564748dbeb3a2a34267c9cfb19fa1b5
meyert8034 <meyert8034@my.uwstout.edu>
2023-03-30 14:00:45 -0500

    Starting to set up main menu activity

---
+++ app/src/main/java/nomic/ui/MainMenuActivity.kt
@@ -0,0 +1,31 @@
+package nomic.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import mobile.game.manager.nomic.R
+import mobile.game.manager.nomic.databinding.MainMenuPageBinding
+
+class MainMenuActivity : AppCompatActivity() {
+
+    private var _binding: MainMenuPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = MainMenuPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCreateGame.setOnClickListener {
+            Log.d("Create Game", "Hello")
+        }
+
+        binding.btnJoinGame.setOnClickListener {
+            Log.d("Join Game", "Hello")
+        }
+
+        binding.btnLoadGame.setOnClickListener {
+            Log.d("Load Game", "Hello")
+        }
+    }
+}
