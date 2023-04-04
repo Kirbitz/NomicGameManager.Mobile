1485b164184678755cfa20a5a9f826108f14473b
Kirbitz <tyler.marefke@gmail.com>
2023-04-04 14:44:22 -0500

    View Model is getting data and activity moved out of main

--- app/src/main/java/nomic/ui/MainActivity.kt
+++ app/src/main/java/nomic/ui/MainActivity.kt
@@ -1,39 +1,23 @@
 package nomic.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
-import kotlinx.coroutines.*
 import mobile.game.manager.nomic.databinding.ActivityMainBinding
-import nomic.data.repositories.NomicApiRepository
-import nomic.ui.viewmodels.RulesListViewModel
-import nomic.ui.viewmodels.RulesListViewModelFactory
+import nomic.ui.activities.RulesListActivity
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val viewModel: RulesListViewModel by viewModels {RulesListViewModelFactory(1, this)}
 
         binding.button.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-//                try {
-//                    val data = nomicApiRepository.getRulesAmendmentsList(2, "Collection").toString()
-//                    withContext(Dispatchers.Main) {
-//                        Toast.makeText(applicationContext, data, Toast.LENGTH_LONG).show()
-//                    }
-//                } catch (exception: Exception) {
-//                    withContext(Dispatchers.Main) {
-//                        Toast.makeText(applicationContext, "Error Happened", Toast.LENGTH_LONG).show()
-//                    }
-//                }
-            }
+            val intent = Intent(this, RulesListActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/nomic/ui/activities/RulesListActivity.kt
@@ -0,0 +1,32 @@
+package nomic.ui.activities
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import mobile.game.manager.nomic.databinding.RulesListBinding
+import nomic.ui.viewmodels.RulesListViewModel
+import nomic.ui.viewmodels.RulesListViewModelFactory
+
+class RulesListActivity : AppCompatActivity() {
+    private var _binding: RulesListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = RulesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel: RulesListViewModel by viewModels { RulesListViewModelFactory(2, this) }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect()
+            }
+        }
+    }
+}
