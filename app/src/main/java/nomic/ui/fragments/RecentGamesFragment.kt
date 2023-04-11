84174c010d1143872bf00f6d6292b2e6d0225bdc
meyert8034 <meyert8034@my.uwstout.edu>
2023-04-11 15:14:51 -0500

    Got fragment linked and binding to viewmodel

--- app/src/main/java/nomic/ui/fragments/RecentGamesFragment.kt
+++ app/src/main/java/nomic/ui/fragments/RecentGamesFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import mobile.game.manager.nomic.databinding.RecentGamesFragmentBinding
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import mobile.game.manager.nomic.R
 import nomic.data.models.GameDTO
 import nomic.ui.viewmodels.MainMenuViewModel
-import nomic.ui.viewmodels.MainMenuViewModelFactory
+import mobile.game.manager.nomic.databinding.FragmentRecentgamesBinding
 
 class RecentGamesFragment : Fragment() {
 
@@ -22,22 +18,22 @@ class RecentGamesFragment : Fragment() {
         fun newInstance() = RecentGamesFragment()
     }
 
-    private lateinit var viewModel: MainMenuViewModel
+    private lateinit var vm: MainMenuViewModel
+    private lateinit var _binding: FragmentRecentgamesBinding
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this)[MainMenuViewModel::class.java]
-    }
+        vm = activity?.run {
+            ViewModelProvider(this)[MainMenuViewModel::class.java]
+        } ?: throw Exception("Invalid Activity")     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val binding: RecentGamesFragmentBinding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_recentgames, container, false)
-        binding.viewModel = vm//attach your viewModel to xml
+    ): View? {
+        _binding = FragmentRecentgamesBinding.inflate(inflater,container,false)
         return binding.root
-    }    }
-
+    }
 }
