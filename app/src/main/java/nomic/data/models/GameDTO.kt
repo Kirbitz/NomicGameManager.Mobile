8b6b0525f5fd1d26c86bd33e40eb08a68b12cc37
meyert8034 <meyert8034@my.uwstout.edu>
2023-04-11 13:00:36 -0500

    Squashed commit of the following:
    commit 09e11d9787e003fd7a26256eff951562052e93a5
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Sat Apr 8 13:52:07 2023 -0500
    
        Updated Kdocs
    
    commit c4ff9fe0020e17abb479c9fedf81594797179210
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Sat Apr 8 13:44:28 2023 -0500
    
        Updated imports
    
    commit 6aef7f7456828de1fd89007fe80d23e4aed7dcb3
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Sat Apr 8 13:41:34 2023 -0500
    
        Implemented method to call the enactAmendment endpoint
    
    commit 00e10de51bea9a2f50c6b1dc5fa4ec845daa818c
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Sat Apr 8 13:25:48 2023 -0500
    
        Implemented methods to call transmuteRule repealAmendment and createGame endpoints
    
    commit fed328c603d3b628c9cdd3d0905ccee1a8fb167e
    Author: Tyler Marefke <tyler.marefke@gmail.com>
    Date:   Wed Apr 5 22:15:52 2023 -0500
    
        Implemented more api calls to NomicApiRepository
    
    commit 1485b164184678755cfa20a5a9f826108f14473b
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Tue Apr 4 14:44:22 2023 -0500
    
        View Model is getting data and activity moved out of main
    
    commit 3b566e5442ff1108c270567246bb36ba8c60b885
    Merge: 8f49fd0 61c1cfc
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Tue Apr 4 13:28:02 2023 -0500
    
        Merge branch 'NOM-8/feature/RulesAmendmentsList' into NOM-8/feature/api-connection
    
        # Conflicts:
        #	app/build.gradle
        #	app/src/main/java/nomic/ui/MainActivity.kt
    
    commit 61c1cfcbdac9b907043863b31d58aed4182e92a4
    Author: Garrett Preston <19gprest@gmail.com>
    Date:   Tue Apr 4 13:25:04 2023 -0500
    
        Pushing latest to give Marefke a copy
    
    commit a30a71f5e00bb292ba5a9dfc4adf43d11b9ceb2c
    Author: Garrett Preston <43272523+Garrett-Preston@users.noreply.github.com>
    Date:   Tue Apr 4 12:34:19 2023 -0500
    
        Nom 8/feature/api connection (#4)
    
        * Update config for local connection to api
    
        * NomicApiRepository now works with Jackson library for JSON deserialization and coroutine for data collection
    
        * Naming updates
    
        ---------
    
        Co-authored-by: Kirbitz <tyler.marefke@gmail.com>
    
    commit a099e46ede9138ee34368501f87ac63fa8602223
    Author: Garrett Preston <19gprest@gmail.com>
    Date:   Tue Apr 4 12:26:33 2023 -0500
    
        Pushing latest to merge the data layer
    
    commit 8f49fd07c073cde53bada4c85f76d7de89205359
    Author: Tyler Marefke <tyler.marefke@gmail.com>
    Date:   Mon Apr 3 20:59:40 2023 -0500
    
        Naming updates
    
    commit a8a0745b12c77f0575c8e3738bbf20a7311bb7e9
    Author: Garrett Preston <19gprest@gmail.com>
    Date:   Mon Apr 3 20:58:51 2023 -0500
    
        Define functionality for RulesListViewModel.kt - mostly not implemented yet
    
    commit 1b68e3977f05fe9849eda03861782a20803d49db
    Author: Tyler Marefke <tyler.marefke@gmail.com>
    Date:   Sun Apr 2 21:38:08 2023 -0500
    
        NomicApiRepository now works with Jackson library for JSON deserialization and coroutine for data collection
    
    commit a9aeb0698eba2f503d75d71d00fcc572ef4231d8
    Author: Kirbitz <tyler.marefke@gmail.com>
    Date:   Sun Apr 2 15:24:03 2023 -0500
    
        Update config for local connection to api

---
+++ app/src/main/java/nomic/data/models/GameDTO.kt
@@ -0,0 +1,20 @@
+package nomic.data.models
+
+import java.time.LocalDate
+
+/**
+ * Data class for game
+ *
+ * @property gameId the unique id of the game
+ * @property title the display title of a game
+ * @property createDate the date when the game was created
+ * @property currentPlayer the indicator of who's turn it is
+ * @property userId the user the game is attached to
+ */
+data class GameDTO (
+    val gameId: Int? = null,
+    val title: String,
+    val createDate: LocalDate? = null,
+    val currentPlayer: Int? = null,
+    val userId: Int
+)
---
+++ app/src/main/java/nomic/data/repositories/INomicApiRepository.kt
@@ -0,0 +1,76 @@
+package nomic.data.repositories
+
+import nomic.data.models.AmendmentDTO
+import nomic.data.models.GameDTO
+import nomic.data.models.RuleDTO
+import nomic.data.models.RulesAmendmentsDTO
+
+/**
+ * Repository for API calls to the Nomic API
+ */
+interface INomicApiRepository {
+
+    /**
+     * Collects the rules and amendments data from the Nomic Api
+     *
+     * @param gameId the game Id to collect rules and amendments data on
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return The list of [RulesAmendmentsModel][nomic.data.models.RulesAmendmentsDTO] objects
+     */
+    suspend fun getRulesAmendmentsList(gameId: Int, tag: String): List<RulesAmendmentsDTO>
+
+    /**
+     * Passes new rule data to the Nomic API
+     *
+     * @param newRule the rule data to be sent to the Nomic API
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String the signifies the rule was created
+     */
+    suspend fun enactRule(newRule: RuleDTO, tag: String): String
+
+    /**
+     * Sends rule data to be repealed to the Nomic API
+     *
+     * @param ruleId the rule Id to repeal from the database
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String that signifies the rule was repealed
+     */
+    suspend fun repealRule(ruleId: Int, tag: String): String
+
+    /**
+     * Sends rule data to be transmuted to the Nomic API
+     *
+     * @param ruleId the rule Id to repeal from the database
+     * @param mutable the flag to update the rules mutability to
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String that signifies the rule was repealed
+     */
+    suspend fun transmuteRule(ruleId: Int, mutable: Boolean, tag: String): String
+
+    /**
+     * Sends amendment data to be enacted to the Nomic API
+     *
+     * @param newAmendment the amendment data to be sent to the Nomic API
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String that signifies the rule was repealed
+     */
+    suspend fun amendRule(newAmendment: AmendmentDTO, tag: String): String
+
+    /**
+     * Sends amendment id to be repealed to the Nomic API
+     *
+     * @param amendId the rule Id to repeal from the database
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String that signifies the rule was repealed
+     */
+    suspend fun repealAmendment(amendId: Int, tag: String): String
+
+    /**
+     * Sends game data to be created to the Nomic API
+     *
+     * @param newGame the game data to be sent to the Nomic API
+     * @param tag Identifier for a request made through the Volley Queue
+     * @return String that signifies the rule was repealed
+     */
+    suspend fun createGame(newGame: GameDTO, tag: String): String
+}
