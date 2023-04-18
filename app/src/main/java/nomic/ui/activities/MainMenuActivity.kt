2e8cdc871d2729ca2c9e4346b003a2076e7d59de
meyert8034 <meyert8034@my.uwstout.edu>
2023-04-18 14:00:36 -0500

    Fleshed out the different class files

--- app/src/main/java/nomic/ui/activities/MainMenuActivity.kt
+++ app/src/main/java/nomic/ui/activities/MainMenuActivity.kt
@@ -5,25 +5,33 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import mobile.game.manager.nomic.R
 import mobile.game.manager.nomic.databinding.MainMenuPageBinding
+import nomic.ui.fragments.RecentGameAdapter
+import nomic.ui.fragments.RecentGamesFragment
 import nomic.ui.viewmodels.MainMenuViewModel
 import nomic.ui.viewmodels.MainMenuViewModelFactory
-import nomic.ui.viewmodels.RulesListViewModel
 
 class MainMenuActivity : AppCompatActivity() {
 
     private var _binding: MainMenuPageBinding? = null
     private val binding get() = _binding!!
+    private lateinit var recentGamesAdapter: RecentGameAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = MainMenuPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+        replaceFragment(RecentGamesFragment())
 
         val viewModel: MainMenuViewModel by viewModels { MainMenuViewModelFactory(2, this) }
 
@@ -45,5 +53,11 @@ class MainMenuActivity : AppCompatActivity() {
             val intent = Intent(this, RulesListActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.rvRecentGames, fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/nomic/ui/fragments/RecentGamesFragment.kt
+++ app/src/main/java/nomic/ui/fragments/RecentGamesFragment.kt
@@ -1,27 +1,32 @@
 package nomic.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import mobile.game.manager.nomic.R
 import mobile.game.manager.nomic.databinding.FragmentRecentgamesBinding
+import mobile.game.manager.nomic.databinding.MainMenuPageBinding
 import nomic.data.models.GameDTO
 import nomic.ui.viewmodels.MainMenuViewModel
 
 class RecentGamesFragment : Fragment() {
 
     private lateinit var viewModel: MainMenuViewModel
-    private lateinit var _binding: FragmentRecentgamesBinding
+    private lateinit var _binding: MainMenuPageBinding
     private val binding get() = _binding
+    private lateinit var recentGamesAdapter: RecentGameAdapter
 
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = activity?.run {
-            ViewModelProvider(this)[MainMenuViewModel::class.java]
-        } ?: throw Exception("Invalid Activity")
+        viewModel = this.context?.let { MainMenuViewModel(1, it) }!!
+        recentGamesAdapter = RecentGameAdapter(viewModel.getGames())
     }
 
     override fun onCreateView(
@@ -29,9 +34,10 @@ class RecentGamesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRecentgamesBinding.inflate(inflater, container, false)
+        _binding = MainMenuPageBinding.inflate(inflater, container, false)
         return binding.root
     }
+
 }
 
 class RecentGameAdapter(
@@ -41,12 +47,16 @@ class RecentGameAdapter(
     private lateinit var _binding: FragmentRecentgamesBinding
     private val binding get() = _binding
 
-    class RecentGameViewHolder(private val binding: FragmentRecentgamesBinding) : RecyclerView.ViewHolder(binding.root)
+    class RecentGameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentGameViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = FragmentRecentgamesBinding.inflate(layoutInflater, parent, false)
-        return RecentGameViewHolder(binding)
+        return RecentGameViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.fragment_recentgames,
+                parent,
+                false,
+            )
+        )
     }
 
     override fun getItemCount(): Int {
