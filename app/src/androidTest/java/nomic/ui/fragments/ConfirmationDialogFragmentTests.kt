16fe69e11adaa9bba807b1e73db38403a72fb74d
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-27 13:59:28 -0500

    Expands tests

--- app/src/androidTest/java/nomic/ui/fragments/ConfirmationDialogFragmentTests.kt
+++ app/src/androidTest/java/nomic/ui/fragments/ConfirmationDialogFragmentTests.kt
@@ -2,8 +2,15 @@ package nomic.ui.fragments
 
 import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragment
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth.assertThat
+import nomic.mobile.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,11 +21,42 @@ class ConfirmationDialogFragmentTests {
     fun test_dialog_containsTitle() {
         val titles = arrayOf("x", "Foo bar", "Hello world")
         for (title in titles) {
-            val scenario = launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", title)))
-            scenario.onFragment { fragment ->
-                assertThat(fragment.confirmationText).isNotNull()
-                assertThat(fragment.confirmationText.text).isEqualTo(title)
-            }
+            launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", title)))
+            onView(withText(title))
+                .check(matches(isDisplayed()))
         }
     }
+
+    @Test
+    fun test_dialog_hasConfirmation() {
+        launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", "Are you sure?")))
+        onView(withText(R.string.confirm)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_dialog_hasCancellation() {
+        launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", "Are you sure?")))
+        onView(withText(R.string.cancel)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_dialog_cancel_dismissesDialog() {
+        val scenario = launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", "Are you sure?")))
+        scenario.moveToState(Lifecycle.State.RESUMED)
+
+        onView(withText(R.string.cancel))
+            .perform(click())
+            .check(doesNotExist())
+    }
+
+    @Test
+    fun test_dialog_confirm_dismissesDialog() {
+        val scenario = launchFragment<ConfirmationDialogFragment>(bundleOf(Pair("confirmationText", "Are you sure?")))
+        scenario.moveToState(Lifecycle.State.RESUMED)
+
+        onView(withText(R.string.confirm))
+            .perform(click())
+            .check(doesNotExist())
+    }
+
 }
