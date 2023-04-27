5e852cd8426c71a48dcba6586e182fe520f0e411
Ben Lamkin <66689498+TolkienFan17@users.noreply.github.com>
2023-04-27 13:07:18 -0500

    Merges with repositories pull request

--- app/src/main/java/nomic/ui/fragments/ConfirmationDialogFragment.kt
+++ app/src/main/java/nomic/ui/fragments/ConfirmationDialogFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import mobile.game.manager.nomic.R
+import nomic.mobile.R
 
 class ConfirmationDialogFragment: BottomSheetDialogFragment() {
 
